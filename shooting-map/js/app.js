a103a7d2293f66200f4b7c9fb5b4202135a03678
kaiyoshino <kaiyosh@gmail.com>
2015-10-16 17:11:22 -0700

    adds layered groups based on race of victim

--- shooting-map/js/app.js
+++ shooting-map/js/app.js
@@ -11,14 +11,54 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoia2FpeW9zaCIsImEiOiJjaWZ0eHVhenExZXdodWhtMmEzZDA1dDB6In0.PEbxwlAVJP5m-c7MqnTqKA'
 }).addTo(map);
 
+var unknown = L.layerGroup(),
+	white = L.layerGroup(),
+	black = L.layerGroup(),
+	asian = L.layerGroup(),
+	indian = L.layerGroup(),
+	pacific = L.layerGroup(),;
+
+var shootingPopUp = {
+	lat: '';
+	lng: '';
+	sum: '';
+}
+
+
 var doSomethingWithData = function(data) {
 	data.forEach(function(shooting) {
-		var circle = L.circle([shooting.lat, shooting.lng], 500, {
+
+		var marker = L.circle([shooting.lat, shooting.lng], 500, {
 		    color: 'red',
 		    fillColor: '#f03',
 		    fillOpacity: 0.5
 		}).addTo(map);
+
+		shootingPopUp.lat = shooting.lat;
+		shootingPopUp.lng = shooting.lng;
+		shootingPopUp.sum = shooting.summary;
+
+		if(race == "Uknown") {
+			marker.addTo(unknown);
+		} else if (race == "White") {
+			marker.addTo(white);
+		} else if (race == "Black or African American") {
+			marker.addTo(black);
+		} else if (race == "Asian") {
+			marker.addTo(asian);
+		} else if (race == "American Indian or Alaska Native") {
+			marker.addTo(indian);
+		} else if (race == "Native Hawaiian or Other Pacific Islander") {
+			marker.addTo(pacific);
 	});
+
+	L.control.layers(unknown, white, black, asian, indian, pacific).addTo(map);
 }
 
 $.getJSON('data/data.min.json').then(doSomethingWithData);
+
+map.on('click', onMapClick)
+
+function onMapClick(shootingPopUp) {
+	shootingPopUp.lat
+}
