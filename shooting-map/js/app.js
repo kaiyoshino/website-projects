f76ada02cb015de0c8fa52285e7be9007cb193f2
kaiyoshino <kaiyosh@gmail.com>
2015-10-17 11:30:24 -0700

    categorizes markers

--- shooting-map/js/app.js
+++ shooting-map/js/app.js
@@ -1,4 +1,3 @@
-/* add your script methods and logic here */
 
 'use strict';
 
@@ -11,61 +10,60 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1Ijoia2FpeW9zaCIsImEiOiJjaWZ0eHVhenExZXdodWhtMmEzZDA1dDB6In0.PEbxwlAVJP5m-c7MqnTqKA'
 }).addTo(map);
 
-var unknown = L.layerGroup();
-var	white = L.layerGroup();
-var	black = L.layerGroup();
-var	asian = L.layerGroup();
-var	indian = L.layerGroup();
-var	pacific = L.layerGroup();
+var unknown = L.layerGroup([]);
+var	white = L.layerGroup([]);
+var	black = L.layerGroup([]);
+var	asian = L.layerGroup([]);
+var	indian = L.layerGroup([]);
+var	pacific = L.layerGroup([]);
 
-// var shootingPopUp = {
-// 	lat: '',
-// 	lng: '',
-// 	sum: ''
-// };
+var overlayMaps = {
+	"Uknown": unknown,
+	"White": white,
+	"Black or African American": black,
+	"Asian": asian,
+	"American Indian or Alaska Native": indian,
+	"Native Hawaiian or Other Pacific Islander": pacific
+}
 
+L.control.layers(overlayMaps).addTo(map);
 
 var doSomethingWithData = function(data) {
 	data.forEach(function(shooting) {
 
+		var markerColor;
+		if (shooting.victim.age < 18) {
+	    	markerColor = 'black'
+		} else if (shooting.victim.age < 25) {
+			markerColor = 'blue'
+		} else {
+			markerColor = 'red'
+		};
+
 		var marker = L.circle([shooting.lat, shooting.lng], 500, {
-		    color: 'red',
+			color: markerColor,
 		    fillColor: '#f03',
-		    fillOpacity: 0.5
-		}).bindPopup(shooting.summary + "(link)".link(shooting.sourceURL)).addTo(map);
-
-		// shootingPopUp.lat = shooting.lat;
-		// shootingPopUp.lng = shooting.lng;
-		// shootingPopUp.sum = shooting.summary;
+		    fillOpacity: 0.25
+		}).bindPopup(shooting.summary + "(link)".link(shooting.sourceURL));
 
 
-
-		if(shooting.race == "Uknown") {
+		if(shooting.victim.race == "Unknown" || shooting.victim.race == null) {
 			marker.addTo(unknown);
-		} else if (shooting.race == "White") {
+		} else if (shooting.victim.race == "White") {
 			marker.addTo(white);
-		} else if (shooting.race == "Black or African American") {
+		} else if (shooting.victim.race == "Black or African American") {
 			marker.addTo(black);
-		} else if (shooting.race == "Asian") {
+		} else if (shooting.victim.race == "Asian") {
 			marker.addTo(asian);
-		} else if (shooting.race == "American Indian or Alaska Native") {
+		} else if (shooting.victim.race == "American Indian or Alaska Native") {
 			marker.addTo(indian);
-		} else if (shooting.race == "Native Hawaiian or Other Pacific Islander") {
+		} else if (shooting.victim.race == "Native Hawaiian or Other Pacific Islander") {
 			marker.addTo(pacific);
 		}
 	});
 
-	L.control.layers(unknown, white, black, asian, indian, pacific).addTo(map);
+
 }
 
+
 $.getJSON('data/data.min.json').then(doSomethingWithData);
-
-
-// function onMapClick(shootingPopUp) {
-// 	var popup = L.popup()
-// 		.setLatLng([shootingPopUp.lat, shootingPopUp.lng])
-// 		.setContent(shootingPopUp.sum)
-// 		.openOn(map);
-// }
-
-// map.on('click', onMapClick)
