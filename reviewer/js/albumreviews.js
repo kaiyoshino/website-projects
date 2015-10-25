c295ca8488fcd55658a81a9b6a8a90615df20aa7
kaiyoshino <kaiyosh@gmail.com>
2015-10-25 16:47:01 -0700

    adds appropriate files and imports libraries

---
+++ reviewer/js/albumreviews.js
@@ -0,0 +1,28 @@
+'use strict';
+
+$('#rating').raty();
+
+Parse.initialize("6KZ3eQKMXbXSAXkPeFeYRLkXrOfKZN7ROByAEIRI", "LIRiQTT1stqDPFEYu1pcbAkqeft0sdYY8kBVXS83");
+
+// var Rating = Parse.Object.extend("Rating");
+// var ratingOne = new Rating();
+//   ratingOne.save({rating: "2"});
+//   ratingOne.save({artist: "The Beatles"});
+//   ratingOne.save({title: "Sgt. Pepper's Lonely Hearts Club Band"})
+//   ratingOne.save({comment: "Mediocre imo."})
+
+// var user = new Parse.User();
+// user.set("username", "Kai");
+// user.set("password", "matterhrn5");
+// user.set("email", "friedjuice@hotmail.com");
+// user.set("phone", "650-555-0000");
+  
+// user.signUp(null, {
+//   success: function(user) {
+//     // Hooray! Let them use the app now.
+//   },
+//   error: function(user, error) {
+//     // Show the error message somewhere and let the user try again.
+//     alert("Error: " + error.code + " " + error.message);
+//   }
+// });
