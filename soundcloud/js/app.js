013d275a95d65c515446067a7a0ae5b77d4a733b
kaiyoshino <kaiyosh@gmail.com>
2015-11-02 21:23:36 -0800

    testing soundcloud api on studetn server

--- soundcloud/js/app.js
+++ soundcloud/js/app.js
@@ -4,14 +4,29 @@ var BASE_URL = 'https://api.soundcloud.com'; //website we fetch information from
 var CLIENT_ID = 'c67c94fac9915ed0ad161e7a8d56cbe0' //application ID for requests
 
 SC.initialize({
-  client_id: 'CLIENT_ID'
+  client_id: 'c67c94fac9915ed0ad161e7a8d56cbe0',
   redirect_uri: 'http://http://students.washington.edu/kaiyosh/info343/challenges/soundcloud/'
 });
+
+// var iframeElement   = document.querySelector('iframe');
+// var iframeElementID = iframeElement.id;
+// var widget1         = SC.Widget(iframeElement);
+// var widget2         = SC.Widget(iframeElementID);
 
 SC.connect().then(function() {
   return SC.get('/me');
 }).then(function(me) {
   alert('Hello, ' + me.username);
+});
+
+// SC.get('chancetherapper/tracks').then(function(tracks){
+//   alert('Latest track: ' + tracks[0].title);
+// });
+
+var track_url = 'http://soundcloud.com/forss/flickermood';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
+  player.play();
 });
 
 // soundcloud.addEventListener('onPlayerReady', function(player, data) {
@@ -36,13 +51,6 @@ SC.connect().then(function() {
 // swfobject.embedSWF("http://player.soundcloud.com/player.swf", "myContent", "81", "100%", "9.0.0","expressInstall.swf", flashvars, params, attributes);
 // var track_url = 'http://soundcloud.com/forss/flickermood';
 
-// SC.oEmbed(track_url, { auto_play: true }, document.getElementById("player")).then(function(oEmbed) {
-//   console.log('oEmbed response: ', oEmbed);
-// });
-
-SC.stream('/tracks/293').then(function(player){
-  player.play();
-});
 
 var myApp = angular.module('myApp', [])
   .controller('MyCtrl', ['$scope', '$http', function($scope, $http) { 
@@ -54,7 +62,6 @@ var myApp = angular.module('myApp', [])
 			$http.get(request) //Angular AJAX call
 			.then(function (response) {
 				$scope.tracks = response.data; //save results to available model
-				console.log($scope.tracks);
 			});
 		}
     };
