7f5c97cf4c260ce218a86241bf35e4a199a8fd24
kaiyoshino <kaiyosh@gmail.com>
2015-11-03 19:35:59 -0800

    adds a play button that plays to widget

--- soundcloud/js/app.js
+++ soundcloud/js/app.js
@@ -8,55 +8,25 @@ SC.initialize({
   redirect_uri: 'http://http://students.washington.edu/kaiyosh/info343/challenges/soundcloud/'
 });
 
-  (function(){
-    var widgetIframe = document.getElementById('sc-widget'),
-        widget       = SC.Widget(widgetIframe),
-        newSoundUrl = 'http://api.soundcloud.com/tracks/13692671';
-
-    widget.bind(SC.Widget.Events.READY, function() {
-      // load new widget
-      $('#play').click(function() {
-        widget.load(newSoundUrl, {
-          show_artwork: false
-        });
-      });
-    });
-
-  }());
-
-  
-
-// var track_url = 'http://soundcloud.com/forss/flickermood';
-// SC.oEmbed(track_url, document.getElementById('player')).then(function(oEmbed) {
-//   player.play();
-// });
-
-
-// var flashvars = {
-//   enable_api: true,
-//   object_id: "myPlayer",
-//   url: "http://soundcloud.com/forss/flickermood"
-// };
-
-// var params = {
-//   allowscriptaccess: "always"
-// };
-
-// var attributes = {
-//   id: "myPlayer",
-//   name: "myPlayer"
-// };
-
-// swfobject.embedSWF("http://player.soundcloud.com/player.swf", "myContent", "81", "100%", "9.0.0","expressInstall.swf", flashvars, params, attributes);
-// var track_url = 'http://soundcloud.com/forss/flickermood';
 
 
 var myApp = angular.module('myApp', [])
   .controller('MyCtrl', ['$scope', '$http', function($scope, $http) { 
+
+  	$scope.doit= function(uri){
+  		var widgetIframe = document.getElementById('sc-widget'),
+        widget = SC.Widget(widgetIframe);
+	    widget.bind(SC.Widget.Events.READY, function() {
+	        widget.load(uri, {
+            	show_artwork: false,
+            	auto_play: true
+        	});
+	    });
+	}
   
     //function called to fetch tracks based on the scope's query
     $scope.getTracks = function() {
-    	if ($('#search').val().length > 0 ) {
+    	if ($('#search').val().length > 1 ) {
 			var request = BASE_URL + '/tracks' + '?' +'client_id='+ CLIENT_ID + '&q=' + $scope.query; //build the RESTful request
 			$http.get(request) //Angular AJAX call
 			.then(function (response) {
@@ -64,4 +34,5 @@ var myApp = angular.module('myApp', [])
 			});
 		}
     };
+
 }])
