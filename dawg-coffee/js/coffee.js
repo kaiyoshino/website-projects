99af1857520fcfffb2a726472b8790512dffea77
kaiyoshino <kaiyosh@gmail.com>
2015-11-08 18:56:45 -0800

    adds nested orders page

---
+++ dawg-coffee/js/coffee.js
@@ -0,0 +1,54 @@
+'use strict';
+
+angular.module('CoffeeApp', ['ui.router'])
+.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) { 
+
+  	$scope.getBeans = function(data) {
+		$http.get('data/products.json').then(function(response) {
+		 		$scope.beans = response.data;
+		 	});
+
+		 	// $scope.saveBean = function(bean) {
+		 	// 	bean.title = bean.name;
+		 	// 	bean.desc = bean.description;
+		 	// 	bean.cost = bean.price;
+		 	// 	watchList.saveFilm(movie);
+		 	// 	console.log("saved ")
+		 	// 	console.log(movie);
+		 	// }
+		 	
+  //   	if ($('#search').val().length > 0 ) {
+		// 	// $http.get(request) //Angular AJAX call
+		// 	// .then(function (response) {
+		// 	// 	$scope.beans = response.data; //save results to available model
+		// 	// });
+		// } else {
+
+		// }
+	}
+
+}])
+
+.config(function($stateProvider){
+	$stateProvider
+		.state('home', {
+			url: '/',
+			templateUrl: 'index.html',
+			controller: 'HomeCtrl'
+		})
+		.state('orders', {
+			url: '/orders',
+			templateUrl: 'order.html',
+			controller: 'OrdersCtrl'
+		})
+})
+
+.config(function($urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+})
+
+.controller('OrdersCtrl', ['$scope', '$http', function($scope, $http) {
+
+
+
+}])
